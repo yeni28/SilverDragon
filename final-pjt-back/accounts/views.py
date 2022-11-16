8c9882ca256fc8c99eb0e959d7325d69fad700be
seungmintheDragon <yongsm295@gmail.com>
2022-11-16 14:04:54 +0900

    1116

--- final-pjt-back/accounts/views.py
+++ final-pjt-back/accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+@api_view(['POST'])
+def signup(request):
+    print(request.data)
+    password = request.data.get("password")
+    password2 = request.data.get("password2")
+
+
+    if password != password2:
+        return Response({"error:비밀번호가 일치하지 않습니다"},status=status.HTTP_400_BAD_REQUEST)
+    userinfo = {
+        'username': request.data["username"],
+        'password' : password
+    }
+
+    serializer = UserSerializer(data = request.data)
+
+    if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            # 사용자의 암호를 해쉬함수를 통해 바꿔줌(암호화)
+            user.set_password(request.data.get("password"))
+            user.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
