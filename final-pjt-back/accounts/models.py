705bfaf98c4b3f74b0d6d9d27b79676e99571c24
yong seung min <yongsm295@gmail.com>
2022-11-21 09:35:24 +0900

    좋아요 모델

--- final-pjt-back/accounts/models.py
+++ final-pjt-back/accounts/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from movies.models import *
+
 
 # Create your models here.
 
 
+
+
 class User(AbstractUser):
-    
     pass
+
+
+
+
+
+
--- final-pjt-back/accounts/serializers.py
+++ final-pjt-back/accounts/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from .models import *
 
 class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = get_user_model()
         fields = ('username',)
+
+
